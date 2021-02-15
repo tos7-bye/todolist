88b8a4f78a67f3fb37b17481a527952824b91e91
TT <tos.10041105@icloud.com>
2021-02-15 16:13:09 +0900

    add staticpages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  get '/users/:id', to: 'users#show', as: 'user'
+  
+  get 'static_page/home'
+  get 'static_page/help'
   root 'top#index'
+
   get 'top/show'
 
   resources :list, only: %i(new create edit update destroy) do
